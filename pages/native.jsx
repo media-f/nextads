bfba6fcf6d6b85b121875645fc3efc7b71e199cf
Stéphane Broquet <stephane@broquet.ch>
2023-11-02 15:06:09 +0100

    adding google halfpage to native page

--- pages/native.jsx
+++ pages/native.jsx
@@ -1,10 +1,24 @@
 import Columns from '../components/columns'
 import { useEffect } from 'react'
+import Script from 'next/script'
 
 export default function Native() {
 
     useEffect(() => {
+        // native :
         if(window.pubonline) window.pubonline.loadAds()
+
+        // google admanager :
+        setTimeout(() => {
+            const { googletag } = window
+            if(googletag.cmd) {
+                googletag.cmd.push(() => {
+                    googletag.pubads().refresh();
+                })
+            }
+    
+        }, 300)
+
       })
 
     return <>
@@ -25,7 +39,33 @@ export default function Native() {
 
     <div data-placement="qbU0VKDGyS" data-items="3"></div>
 
-    <div className='adslot halfpage' id="adnz_halfpage_1"></div>
+    <div className='adslot halfpage' id="lg_halfpage_1"></div>
+
+    <Script
+        id="admanager-script"
+        src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"
+        strategy="lazyOnload"
+    />
+    <Script id="define-slots" strategy="lazyOnload">{`
+        window.googletag = window.googletag || {cmd: []};
+        
+        googletag.cmd.push(function() {
+            let width = window.innerWidth;
+            if (width > 994) {
+                googletag.defineSlot('/95737030/LG_wideboard_1', [[994, 250], [994, 500], [994, 118]], 'lg_wideboard_1').addService(googletag.pubads());
+                googletag.defineSlot('/95737030/LG_wideboard_2', [[994, 250], [994, 500], [994, 118]], 'lg_wideboard_2').addService(googletag.pubads());
+            }
+            else {
+                googletag.defineSlot('/95737030/LG_wideboard_1', [[300,250]], 'lg_wideboard_1').addService(googletag.pubads());
+                googletag.defineSlot('/95737030/LG_wideboard_2', [[300,250], [320,460]], 'lg_wideboard_2').addService(googletag.pubads());
+            }
+            googletag.defineSlot('/95737030/LG_halfpage_1', [[300, 250], [300, 600]], 'lg_halfpage_1').addService(googletag.pubads());
+            googletag.defineSlot('/95737030/LG_rectangle_1', [[320, 460], [300, 250]], 'lg_rectangle_1').addService(googletag.pubads());
+            googletag.pubads().enableSingleRequest();
+            googletag.enableServices();
+        })
+        
+    `}</Script>
        
     </>
 }
