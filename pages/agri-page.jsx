0981debeca5916f50ddb21f1934045c953913f64
Stéphane Broquet <stephane@broquet.ch>
2024-01-04 12:22:08 +0100

    Refactor ad slot definitions and initialization

--- pages/agri-page.jsx
+++ pages/agri-page.jsx
@@ -5,20 +5,41 @@ import Columns from "../components/columns";
 
 export default function Agri() {
   useEffect(() => {
-    setTimeout(() => {
-      const { googletag } = window;
-      if (googletag.cmd) {
-        console.log("displaying ads");
-        googletag.cmd.push(() => {
-          googletag.display("AGRI_WIDEBOARD_1");
-          googletag.display("AGRI_WIDEBOARD_2");
-          googletag.display("AGRI_WIDEBOARD_3");
-          googletag.display("AGRI_WIDEBOARD_4");
-          googletag.display("AGRI_HALFPAGE_1");
-          googletag.pubads().refresh();
-        });
-      }
-    }, 1000);
+    window.googletag = window.googletag || {cmd: []};
+    googletag.destroySlots();
+        
+    googletag.cmd.push(function() {
+        let width = window.innerWidth;
+        if (width > 994) {
+            googletag.defineSlot('/23038965275/AGRI_WIDEBOARD_1', [[994, 250], [994, 500], [994, 118]], 'AGRI_WIDEBOARD_1').addService(googletag.pubads());
+            googletag.defineSlot('/23038965275/AGRI_WIDEBOARD_2', [[994, 250], [994, 500], [994, 118]], 'AGRI_WIDEBOARD_2').addService(googletag.pubads());
+            googletag.defineSlot('/23038965275/AGRI_HALFPAGE_1', [[300, 250], [300, 600]], 'AGRI_HALFPAGE_1').addService(googletag.pubads());
+        }
+        else {
+            googletag.defineSlot('/23038965275/AGRI_WIDEBOARD_1', [[300,250]], 'AGRI_WIDEBOARD_1').addService(googletag.pubads());
+            googletag.defineSlot('/23038965275/AGRI_WIDEBOARD_2', [[300,250], [320,460]], 'AGRI_WIDEBOARD_2').addService(googletag.pubads());
+        }
+        
+        
+        googletag.pubads().disableInitialLoad();
+        googletag.enableServices();
+
+        if (googletag.cmd) {
+          console.log("displaying ads");
+          googletag.cmd.push(() => {
+            googletag.display("AGRI_WIDEBOARD_1");
+            googletag.display("AGRI_WIDEBOARD_2");
+            googletag.display("AGRI_WIDEBOARD_3");
+            googletag.display("AGRI_WIDEBOARD_4");
+            googletag.display("AGRI_HALFPAGE_1");
+            googletag.pubads().refresh();
+          });
+        }
+
+
+    });
+
+
   }, []);
 
   return (
@@ -117,31 +138,7 @@ export default function Agri() {
         src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"
         strategy="lazyOnload"
       />
-      <Script id="define-slots" strategy="lazyOnload">{`
-        window.googletag = window.googletag || {cmd: []};
-        
-        googletag.cmd.push(function() {
-            let width = window.innerWidth;
-            if (width > 994) {
-                googletag.defineSlot('/23038965275/AGRI_WIDEBOARD_1', [[994, 250], [994, 500], [994, 118]], 'AGRI_WIDEBOARD_1').addService(googletag.pubads());
-                googletag.defineSlot('/23038965275/AGRI_WIDEBOARD_2', [[994, 250], [994, 500], [994, 118]], 'AGRI_WIDEBOARD_2').addService(googletag.pubads());
-                googletag.defineSlot('/23038965275/AGRI_WIDEBOARD_3', [[994, 250], [994, 500], [994, 118]], 'AGRI_WIDEBOARD_3').addService(googletag.pubads());
-                googletag.defineSlot('/23038965275/AGRI_WIDEBOARD_4', [[994, 250], [994, 500], [994, 118]], 'AGRI_WIDEBOARD_4').addService(googletag.pubads());
-                googletag.defineSlot('/23038965275/AGRI_HALFPAGE_1', [[300, 250], [300, 600]], 'AGRI_HALFPAGE_1').addService(googletag.pubads());
-            }
-            else {
-                googletag.defineSlot('/23038965275/AGRI_WIDEBOARD_1', [[300,250]], 'AGRI_WIDEBOARD_1').addService(googletag.pubads());
-                googletag.defineSlot('/23038965275/AGRI_WIDEBOARD_2', [[300,250], [320,460]], 'AGRI_WIDEBOARD_2').addService(googletag.pubads());
-                googletag.defineSlot('/23038965275/AGRI_WIDEBOARD_3', [[300,250], [320,460]], 'AGRI_WIDEBOARD_3').addService(googletag.pubads());
-                googletag.defineSlot('/23038965275/AGRI_WIDEBOARD_4', [[300,250], [320,460]], 'AGRI_WIDEBOARD_4').addService(googletag.pubads());
-            }
-            
-            
-            googletag.pubads().disableInitialLoad();
-            googletag.enableServices();
-        })
-        
-    `}</Script>
+
     </>
   );
 }
