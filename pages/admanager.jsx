85149f08d0fd3b77c6be12866029f9f60be9d5e0
Stéphane Broquet <stephane@broquet.ch>
2024-01-04 12:56:43 +0100

    Refactor ad slot definitions and ad loading logic

--- pages/admanager.jsx
+++ pages/admanager.jsx
@@ -4,19 +4,40 @@ import Columns from "../components/columns";
 
 export default function AdManager() {
   useEffect(() => {
-    setTimeout(() => {
-      const { googletag } = window;
-      if (googletag.cmd) {
+    window.googletag = window.googletag || {cmd: []};
+
+    // destroy GPT ads slots
+    if (window.googletag && typeof window.googletag.destroySlots === "function") {
+      window.googletag.destroySlots();
+      console.log('destroy ad slots')
+    }
+        
+    googletag.cmd.push(function() {
+      //define ad slots present on the page
+        let width = window.innerWidth;
+        if (width > 994) {
+            googletag.defineSlot('/95737030/LG_wideboard_1', [[994, 250], [994, 500], [994, 118]], 'lg_wideboard_1').addService(googletag.pubads());
+            googletag.defineSlot('/95737030/LG_wideboard_2', [[994, 250], [994, 500], [994, 118]], 'lg_wideboard_2').addService(googletag.pubads());
+        }
+        else {
+            googletag.defineSlot('/95737030/LG_wideboard_1', [[300,250]], 'lg_wideboard_1').addService(googletag.pubads());
+            googletag.defineSlot('/95737030/LG_wideboard_2', [[300,250], [320,460]], 'lg_wideboard_2').addService(googletag.pubads());
+        }
+        googletag.defineSlot('/95737030/LG_halfpage_1', [[300, 250], [300, 600]], 'lg_halfpage_1').addService(googletag.pubads());
+        googletag.defineSlot('/95737030/LG_rectangle_1', [[320, 460], [300, 250]], 'lg_rectangle_1').addService(googletag.pubads());
+        googletag.pubads().disableInitialLoad();
+        googletag.enableServices();
+
+        // load ads
         console.log("displaying ads");
-        googletag.cmd.push(() => {
-          googletag.display("lg_wideboard_1");
-          googletag.display("lg_wideboard_2");
-          googletag.display("lg_halfpage_1");
-          googletag.display("lg_rectangle_1");
-          googletag.pubads().refresh();
-        });
-      }
-    }, 1000);
+        googletag.display("lg_wideboard_1");
+        googletag.display("lg_wideboard_2");
+        googletag.display("lg_halfpage_1");
+        googletag.display("lg_rectangle_1");
+        googletag.pubads().refresh();
+
+    });
+
   }, []);
 
   return (
@@ -48,26 +69,6 @@ export default function AdManager() {
         src="https://securepubads.g.doubleclick.net/tag/js/gpt.js"
         strategy="lazyOnload"
       />
-      <Script id="define-slots" strategy="lazyOnload">{`
-        window.googletag = window.googletag || {cmd: []};
-        
-        googletag.cmd.push(function() {
-            let width = window.innerWidth;
-            if (width > 994) {
-                googletag.defineSlot('/95737030/LG_wideboard_1', [[994, 250], [994, 500], [994, 118]], 'lg_wideboard_1').addService(googletag.pubads());
-                googletag.defineSlot('/95737030/LG_wideboard_2', [[994, 250], [994, 500], [994, 118]], 'lg_wideboard_2').addService(googletag.pubads());
-            }
-            else {
-                googletag.defineSlot('/95737030/LG_wideboard_1', [[300,250]], 'lg_wideboard_1').addService(googletag.pubads());
-                googletag.defineSlot('/95737030/LG_wideboard_2', [[300,250], [320,460]], 'lg_wideboard_2').addService(googletag.pubads());
-            }
-            googletag.defineSlot('/95737030/LG_halfpage_1', [[300, 250], [300, 600]], 'lg_halfpage_1').addService(googletag.pubads());
-            googletag.defineSlot('/95737030/LG_rectangle_1', [[320, 460], [300, 250]], 'lg_rectangle_1').addService(googletag.pubads());
-            googletag.pubads().disableInitialLoad();
-            googletag.enableServices();
-        })
-        
-    `}</Script>
     </>
   );
 }
