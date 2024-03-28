a0446a0e61cdc8b8b7f9727717f6ab979552b726
Stéphane Broquet <stephane@broquet.ch>
2024-03-28 15:07:07 +0100

    added Google Tag Manager

--- components/layout.js
+++ components/layout.js
@@ -1,10 +1,13 @@
 import '@picocss/pico'
 import Menu from './menu'
 import Script from 'next/script'
+import { GoogleTagManager } from '@next/third-parties/google'
+
 
 export default function Layout({ children }) {
     return (
       <>
+        <GoogleTagManager gtmId="GTM-WXNS2L4C" />
         
         <main className="container">
           {children}
