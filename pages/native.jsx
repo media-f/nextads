6aea71314b4366086e6c2b2408ee0f7aa6a23a18
Stéphane Broquet <stephane@broquet.ch>
2025-01-20 14:47:45 +0100

    tiktok

--- pages/native.jsx
+++ pages/native.jsx
@@ -29,29 +29,26 @@ export default function Native() {
       <section>
         <h1>Native</h1>
 
-        <Menu></Menu>      
+        <Menu />
 
-        <Columns></Columns>
+        <Columns />
 
-
-        <blockquote className="tiktok-embed" cite="https://www.tiktok.com/@bayashi.tiktok/video/7417752854972140807" data-video-id="7417752854972140807" style={{ maxWidth: "605px", minWidth: "325px" }}>
+        <blockquote className="tiktok-embed" cite="https://www.tiktok.com/@bayashi.tiktok/video/7377663484575616257" data-video-id="7377663484575616257" style={{ maxWidth: "605px", minWidth: "325px" }}>
           <section>
-            <a target="_blank" title="@bayashi.tiktok" href="https://www.tiktok.com/@bayashi.tiktok?refer=embed">@bayashi.tiktok</a>
-            <a title="tiktokfood" target="_blank" href="https://www.tiktok.com/tag/tiktokfood?refer=embed">#tiktokfood</a>
-            <a title="asmr" target="_blank" href="https://www.tiktok.com/tag/asmr?refer=embed">#asmr</a>
+            <a target="_blank" rel="noopener noreferrer" title="@bayashi.tiktok" href="https://www.tiktok.com/@bayashi.tiktok?refer=embed">@bayashi.tiktok</a>
+            <a title="tiktokfood" target="_blank" rel="noopener noreferrer" href="https://www.tiktok.com/tag/tiktokfood?refer=embed">#tiktokfood</a>
+            <a title="asmr" target="_blank" rel="noopener noreferrer" href="https://www.tiktok.com/tag/asmr?refer=embed">#asmr</a>
+            <a target="_blank" rel="noopener noreferrer" title="♬ オリジナル楽曲  - バヤシ🥑Bayashi" href="https://www.tiktok.com/music/オリジナル楽曲-バヤシ🥑Bayashi-7377663521633749776?refer=embed">♬ オリジナル楽曲  - バヤシ🥑Bayashi</a>
           </section>
         </blockquote>
         <Script async src="https://www.tiktok.com/embed.js"></Script>
 
-
-        <Content></Content>
-
         <h3>1 item</h3>
         <div data-placement="qbU0VKDGyS" data-items="1"></div>
 
-        <Columns></Columns>
+        <Columns />
 
-        <Columns></Columns>
+        <Columns />
         <h3>2-3 items</h3>
         <div data-placement="qbU0VKDGyS" data-items="3"></div>
 
@@ -59,21 +56,18 @@ export default function Native() {
           <div className="adslot rectangle" id="lg_rectangle_1"></div>
         </Columns>
 
-        <Columns></Columns>
+        <Columns />
 
         <h3>2-3 items</h3>
         <div data-placement="qbU0VKDGyS" data-items="3"></div>
 
         <h3>2-3 items</h3>
         <div data-placement="qbU0VKDGyS" data-items="3"></div>
-
-        
       </section>
 
       <aside>
         <div className="adslot halfpage" id="lg_halfpage_1"></div>
       </aside>
-
     </>
   );
 }
