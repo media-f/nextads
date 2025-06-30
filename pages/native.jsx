05393665539288dcd1b9c365cb9e60622f080cd1
Stéphane Broquet <stephane@broquet.ch>
2025-06-30 11:23:54 +0200

    Ajout de l'attribut data-cmp-purpose et de la classe cmplazyload à l'iframe pour améliorer la gestion des ressources publicitaires

--- pages/native.jsx
+++ pages/native.jsx
@@ -35,7 +35,7 @@ export default function Native() {
         <Columns />
 
 
-        <iframe height="300" width= "400" src="https://www.tiktok.com/player/v1/7377663484575616257?&music_info=1&description=1" allow="fullscreen" title="test"></iframe>
+        <iframe height="300" width= "400" src="https://www.tiktok.com/player/v1/7377663484575616257?&music_info=1&description=1" allow="fullscreen" title="test" data-cmp-purpose="c2" class="cmplazyload"></iframe>
 
         <h3>1 item</h3>
         <div data-placement="llhome2024" data-items="1"></div>
